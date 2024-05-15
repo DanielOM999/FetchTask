b8c755f8fbd44e410a3cee4f44427fbea58c318c
DanielOM999 <daniel.mostad1@gmail.com>
2024-05-15 20:58:10 +0200

    added all breweries

--- code/menu.js
+++ code/menu.js
@@ -14,23 +14,30 @@ function openmenu() {
 const attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 
 var map = L.map('map1').setView([0, 0], 2);
-let marker = L.marker([59.745164250056135,10.164131070531106 ]).addTo(map)
 let tileURL = 'https://api.maptiler.com/maps/satellite/{z}/{x}/{y}.jpg?key=jrWXhyKc3SrZIgzh4DCp';
-const tiles = L.tileLayer(tileURL, {attribution}).addTo(map)
+const tiles = L.tileLayer(tileURL, {attribution}).addTo(map);
 let place = document.getElementById("searchbar").value;
 const api_url = 'https://nominatim.openstreetmap.org/search?format=json&q=' + place;; 
 console.log(place);
 
-show_me()
+show_me(true, currentPage = 0)
 
-async function show_me(){
-    let response = await fetch("https://api.openbrewerydb.org/v1/breweries/");
+async function show_me(Run, currentPage){
+    let response = await fetch(`https://api.openbrewerydb.org/v1/breweries?page=${currentPage}&per_page=5`);
     let data = await response.json();
-    data.forEach(element => {
-        console.log("Latitude:", element.latitude, "Longitude:", element.longitude);
-        if (element.latitude && element.longitude) {
-            let marker = L.marker([element.latitude, element.longitude]).addTo(map);
-            marker.bindPopup(`<b>${element.name}</b><br>${element.latitude}  ${element.longitude}`).openPopup();
-        }
-    });
+    if (data.length > 0) {
+        data.forEach(element => {
+            console.log("Latitude:", element.latitude, "Longitude:", element.longitude);
+            if (element.latitude && element.longitude) {
+                let marker = L.marker([element.latitude, element.longitude]).addTo(map);
+                marker.bindPopup(`<b>${element.name}</b><br>${element.latitude}  ${element.longitude}`);
+            }
+        });
+    } else {
+        console.log("bu");
+        Run = false;
+    }
+    if (Run) {
+        setTimeout(() => show_me(Run, currentPage + 1), 1000);
+    }
 }
