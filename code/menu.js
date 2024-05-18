90c895dbeb7b02c6fff792a1d832deda63e40194
DanielOM999 <daniel.mostad1@gmail.com>
2024-05-18 16:07:47 +0200

    added it done

--- code/menu.js
+++ code/menu.js
@@ -1,6 +1,19 @@
+// Finds elements by id and adds an eventlistener
 const mybtn = document.getElementById('myList');
 const tre = document.getElementById('btn');
 tre.addEventListener("click", openmenu );
+
+// Initialize map and the icon
+var map = L.map('map1')
+var customIcon = L.icon({
+    iconUrl: '../public/beer.png',
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -32]
+});
+var mapIn = false;
+
+// Toggle menu visibility
 function openmenu() {
     if(mybtn.style.display != 'block') {
         mybtn.style.display = 'block';
@@ -10,10 +23,18 @@ function openmenu() {
     console.log('clicked');
 }
 
-// map settings
-// show_me()
-
+// Makes a search trought and display breweries on the map
 async function search () {
+    if (!mapIn) {
+        document.getElementById("map1").style.backgroundImage = "url(" + "" + ")"
+        document.getElementById("mapH").innerText = ""
+        const attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+
+        map.setView([39.8333, -98.5855], 3);
+        let tileURL = 'https://api.maptiler.com/maps/satellite/{z}/{x}/{y}.jpg?key=jrWXhyKc3SrZIgzh4DCp';
+        const tiles = L.tileLayer(tileURL, {attribution}).addTo(map)
+        mapIn = true;
+    }
     let place = document.getElementById("searchbar").value;
     console.log(place);
     let response = await fetch('https://api.openbrewerydb.org/v1/breweries/search?query=' + place);
@@ -34,33 +55,33 @@ async function search () {
     });
 }
 
+// Function to display all breweries on first page and only 51 on the map
 async function show_me(){
-    const attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    if (!mapIn) {
+        document.getElementById("map1").style.backgroundImage = "url(" + "" + ")"
+        document.getElementById("mapH").innerText = ""
+        const attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 
-    var map = L.map('map1').setView([39.8333, -98.5855], 3);
-    let tileURL = 'https://api.maptiler.com/maps/satellite/{z}/{x}/{y}.jpg?key=jrWXhyKc3SrZIgzh4DCp';
-    const tiles = L.tileLayer(tileURL, {attribution}).addTo(map)
-    var customIcon = L.icon({
-        iconUrl: '../public/beer.png',
-        iconSize: [32, 32],
-        iconAnchor: [16, 32],
-        popupAnchor: [0, -32]
-    });
+        map.setView([39.8333, -98.5855], 3);
+        let tileURL = 'https://api.maptiler.com/maps/satellite/{z}/{x}/{y}.jpg?key=jrWXhyKc3SrZIgzh4DCp';
+        const tiles = L.tileLayer(tileURL, {attribution}).addTo(map)
 
-    let response = await fetch("https://api.openbrewerydb.org/v1/breweries/");
-    let data = await response.json();
+        let response = await fetch("https://api.openbrewerydb.org/v1/breweries?per_page=51");
+        let data = await response.json();
 
-    map.eachLayer(function(layer) {
-        if (layer instanceof L.Marker) {
-            map.removeLayer(layer);
-        }
-    });
+        map.eachLayer(function(layer) {
+            if (layer instanceof L.Marker) {
+                map.removeLayer(layer);
+            }
+        });
 
-    data.forEach(element => {
-        console.log("Latitude:", element.latitude, "Longitude:", element.longitude);
-        if (element.latitude && element.longitude) {
-            let marker = L.marker([element.latitude, element.longitude], {icon: customIcon}).addTo(map);
-            marker.bindPopup(`<b>${element.name}</b><br>${element.latitude}  ${element.longitude}`);
-        }
-    });
+        data.forEach(element => {
+            console.log("Latitude:", element.latitude, "Longitude:", element.longitude);
+            if (element.latitude && element.longitude) {
+                let marker = L.marker([element.latitude, element.longitude], {icon: customIcon}).addTo(map);
+                marker.bindPopup(`<b>${element.name}</b><br>${element.latitude}  ${element.longitude}`);
+            }
+        });
+        mapIn = true
+    }
 }
